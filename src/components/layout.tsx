be7d4ac02425dd1fc5c1bac36944891881b76954
JScoder4005 <muk4005@gmail.com>
2024-11-20 17:04:12 +0530

    Created Theme-provider for light and dark mode by using ShadCn... Created new component and updated routes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,19 @@
-import { PropsWithChildren } from 'react';
+import type { PropsWithChildren } from 'react';
+import Header from './header';
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="bg-gradient-to-br from-background to-muted">
-      {/* header */}
-      <main className="max-h-screen container mx-auto px-4 py-8">
+      <Header />
+      header
+      <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
       </main>
-
-      {/* footer */}
+      <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
+        <div className="container mx-auto px-4 text-center text-gray-200">
+          <p>Made with 💗 by JScoder</p>
+        </div>
+      </footer>
     </div>
   );
 };
