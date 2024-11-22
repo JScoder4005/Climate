58fd051e73d705d4dd2852f14bcc3369709efa25
JScoder4005 <muk4005@gmail.com>
2024-11-22 17:41:53 +0530

    Working on Header, Implamented logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,9 +2,11 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <Link to={'/'}></Link>
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop:filter]:bg-background/60">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to={'/'}>
+          <img src="/logo.png" alt="Climate Logo" className="h-14" />
+        </Link>
       </div>
     </header>
   );
