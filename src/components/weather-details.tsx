e7ea8dfa90c8ab958b998407e0443a6d97789f08
JScoder4005 <muk4005@gmail.com>
2024-11-30 09:43:28 +0530

    Working on Search button functionality, create custom hook to storage search history in local-storage

--- src/components/weather-details.tsx
+++ src/components/weather-details.tsx
@@ -1,10 +1,77 @@
 import type { WeatherData } from '@/api/types';
-
+import { format } from 'date-fns';
+import { Compass, Gauge, Sunrise, Sunset } from 'lucide-react';
+import { Card, CardContent, CardTitle } from './ui/card';
 interface WeatherDetailsProps {
   data: WeatherData;
 }
 const WeatherDetails = ({ data }: WeatherDetailsProps) => {
-  return <div></div>;
+  const { wind, main, sys } = data;
+
+  const formatTime = (timeStamp: number) => {
+    return format(new Date(timeStamp * 1000), 'h:mm a');
+  };
+  // Convert wind degree to direction
+  const getWindDirection = (degree: number) => {
+    const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    const index =
+      Math.round(((degree %= 360) < 0 ? degree + 360 : degree) / 45) % 8;
+    return directions[index];
+  };
+
+  const details = [
+    {
+      title: 'Sunrise',
+      value: formatTime(sys.sunrise),
+      icon: Sunrise,
+      color: 'text-orange-500',
+    },
+    {
+      title: 'Sunset',
+      value: formatTime(sys.sunset),
+      icon: Sunset,
+      color: 'text-blue-500',
+    },
+    {
+      title: 'Wind Direction',
+      value: `${getWindDirection(wind.deg)} (${wind.deg}°)`,
+      icon: Compass,
+      color: 'text-green-500',
+    },
+    {
+      title: 'Pressure',
+      value: `${main.pressure} hPa`,
+      icon: Gauge,
+      color: 'text-purple-500',
+    },
+  ];
+  return (
+    <Card>
+      <CardTitle>Weather Details</CardTitle>
+      <CardContent>
+        <div className="grid sm:grid-cols-2 gap-6">
+          {details.map((detail) => {
+            return (
+              <div
+                key={detail.title}
+                className="flex items-center gap-3 rounded-lg border p-4"
+              >
+                <detail.icon className={`h-5 w-5 ${detail.color}`} />
+                <div>
+                  <p className="text-sm font-medium leading-none">
+                    {detail.title}
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    {detail.value}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default WeatherDetails;
